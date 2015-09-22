9e085ec35f3edf51efc20aa4452bcfcdc1852159
unknown <40170351@ME0MC05-140026.napier-mail.napier.ac.uk>
2015-09-22 15:29:51 +0100

    Added missing files

---
+++ Range Checker/Range Checker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range_Checker {
+    class Program {
+        static int ValidateValue(String number) {
+            int output;
+
+            if (Int32.TryParse(number, out output)) {
+                //Success
+            } else {
+                Console.Write("Please enter a valid value: ");
+            }
+            return output;
+        }
+        static void Main(string[] args) {
+            String numberEntered = "";
+            int numberEnteredInt = 0;
+
+            Console.Write("Enter a value between 1 and 10,000: ");
+
+            while (numberEnteredInt < 1 || numberEnteredInt > 10000) {
+                numberEntered = Console.ReadLine();
+                numberEnteredInt = ValidateValue(numberEntered);
+            }
+
+            for (int x = 2; x < numberEnteredInt; x++)
+            {
+                int isPrime = 0;
+                for (int y = 1; y < x; y++)
+                {
+                    if (x % y == 0)
+                        isPrime++;
+
+                    if (isPrime == 2) break;
+                }
+                if (isPrime != 2)
+                    Console.WriteLine(x);
+
+                isPrime = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Split Values/Split Values/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Values {
+    class Program {
+        static void Main(string[] args) {
+            String valuesEntered = "";
+            int totalCount = 0;
+            int currentNumber = 0;
+            bool valid = false;
+
+            while (valid == false) {
+                Console.Write("Enter a set of valid values seperated by a comma: ");
+                valuesEntered = Console.ReadLine();
+
+                string[] numbers = valuesEntered.Split(',');
+
+                for (int i = 0; i < numbers.Length + 0; i++) {
+                    if (Int32.TryParse(numbers[i], out currentNumber)) {
+                        totalCount += currentNumber;
+                        valid = true;
+                    }
+                    else {
+                        i = numbers.Length;
+                        valid = false;
+                    }
+                }
+            }
+            Console.WriteLine(totalCount);
+            Console.ReadKey();
+        }
+    }
+}
